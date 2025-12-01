97a6e636f0393d83fd615ad8897544eeb4225a74
a-krawcznski <a.krawczynski@wustl.edu>
2025-12-01 17:24:22 -0600

    hi

--- src/exam/codewriting/q1/Dormitory.java
+++ src/exam/codewriting/q1/Dormitory.java
@@ -2,7 +2,6 @@ package exam.codewriting.q1;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
 
 /* We wish to create a Dormitory class. A dormitory has-a(n):
 
@@ -18,31 +17,37 @@ public class Dormitory
 	// Write the constructor for the Dormitory class:
 	// (For full credit the constructor should use only one parameter)
 
+	public Dormitory(String name) {
+		this.name = name;
+		this.rooms = new HashMap<>();
+	}
 
 	//Now, write out the remaining methods:
 	public String getName () {
-		return ""; //FIXME
+		return name;
 	}
 
 	/**
 	 * Returns the amount of rooms that are currently occupied.
 	 */
 	public int getOccupiedRooms () {
-		return 0;//FIXME
+		return rooms.size();
 	}
 	
 	/**
 	* Sets the given room number to be occupied by the given Student.
 	*/
 	public void addStudent ( String roomNo , Student s) {
-		//FIXME
+		rooms.put(roomNo, s);
 	}
 	
 	/**
 	* Returns true if the given room contains a Student, false otherwise
 	*/
 	public boolean isOccupied ( String roomNo ) {
-		return false; //FIXME
+		if (rooms.get(roomNo) != null) {
+			return true;
+		} else return false;
 	}
 	
 	/**
@@ -51,10 +56,23 @@ public class Dormitory
 	* @return A List containing the student ID numbers of all students in this dorm
 	*/
 	public LinkedList < Integer > getStudentIds () {
-		return null; //FIXME
+		LinkedList<Integer> ids = new LinkedList<>();
+		for (Student s : rooms.values()) {
+			ids.add(s.getId());
+		} return ids;
 	}
 	
 	public static void main(String[] args) {
+		Dormitory dorm = new Dormitory("Krawczynski House");
+		Student Alina = new Student(9);
+		dorm.addStudent("110",Alina);
+		LinkedList<Integer> ids = dorm.getStudentIds();
+		if (ids.contains(123456)) {
+			System.out.println("Student is in the dorm.");
+		} else {
+			System.out.println("Student is not in the dorm.");
+		}
+
 		
 		//Create an instance of the Dormitory class and store it in a variable called dorm.
 		
